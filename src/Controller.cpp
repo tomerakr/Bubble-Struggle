6058882c6208e8983bb18cdc613b46d28895a6fb
Netanel <netanelbondar74@gmail.com>
2022-05-17 14:37:16 +0300

    merge

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -5,7 +5,7 @@ constexpr int ropeHeight = 0;
 
 Controller::Controller()
 	: m_window(), m_rope(sf::Vector2f(0,0), sf::Vector2f(ropeWitdh, ropeHeight), Objects::Rope, sf::Color::Red),
-		m_bear(&m_rope), m_window(), m_bear(), m_menu(&m_window)
+		m_bear(&m_rope), m_menu(&m_window)
 {}
 
 void Controller::play()
